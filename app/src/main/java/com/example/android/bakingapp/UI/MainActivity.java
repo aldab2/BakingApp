50e6fec4434a9af49c5ecd1a3111707d401d64f4
Barnawi <ab.barnawi@gmail.com>
2019-05-01 18:16:17 +0300

    Almost done migrating to Fragments

--- app/src/main/java/com/example/android/bakingapp/UI/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/UI/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.bakingapp.UI;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -28,111 +29,27 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements DetailedFragment.OnFragmentClickListener {
-    @BindView(R.id.rv_list_recipe)
-    RecyclerView mRecylerView;
 
-    @BindView(R.id.pb_loading)
-    ProgressBar mProressBar;
-
-    ArrayList<Recipe> dummyRecipes= new ArrayList<>();
-    RecipeAdapter mAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        mAdapter = new RecipeAdapter(dummyRecipes,this);
-        mAdapter.recipes = dummyRecipes;
-        mAdapter.notifyDataSetChanged();
-        ArrayAdapter<Recipe> arrayAdapter = new ArrayAdapter<Recipe>(this,R.layout.recipe_card,dummyRecipes);
-        mRecylerView.setAdapter(mAdapter);
-        int cols = calculateNoOfColumns(this);
-        mRecylerView.setLayoutManager(new GridLayoutManager(this,cols));
-        URL url=null;
-        try {
-             url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
-        }
-        catch (MalformedURLException mue){
-            mue.printStackTrace();
-        }
+        RecipesListFragment recipesListFragment = new RecipesListFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(
+                R.id.fragment_container,recipesListFragment
+        ).commit();
 
-        new AsyncTask<URL,Void,ArrayList<Recipe>>(){
 
-            protected void onPreExecute() {
-                mProressBar.setVisibility(ProgressBar.VISIBLE);
 
-                super.onPreExecute();
-            }
 
-            @Override
-            protected ArrayList<Recipe> doInBackground(URL... urls) {
-                URL url = urls[0];
-                ArrayList<Recipe> recipes =null;
-                try {
-                    String jsonResult = getResponseFromHttpUrl(url);
-                    recipes  = RecipeJsonUtils.reviewJsonParse(jsonResult);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return recipes;
-            }
-
-            @Override
-            protected void onPostExecute(ArrayList<Recipe> recipes) {
-                super.onPostExecute(recipes);
-                mProressBar.setVisibility(View.INVISIBLE);
-                mAdapter = new RecipeAdapter(recipes,getApplicationContext());
-                mRecylerView.setAdapter(mAdapter);
-
-            }
-        }.execute(url);
-        RecipeJsonUtils.reviewJsonParse("");
 
 
     }
-    public static int calculateNoOfColumns(Context context) {
-        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-        int noOfColumns = 0;
-        Log.e("XXXX", "dpWidth = : "+dpWidth );
-        if(dpWidth <=599){
-             noOfColumns=1;
-        }else {
-        int scalingFactor = 200;
-         noOfColumns = (int) (dpWidth / scalingFactor);
-        if(noOfColumns < 2)
-            noOfColumns = 2;
-        }
-        return noOfColumns;
-    }
 
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        Log.d("XXXXXXXX", "getResponseFromHttpUrl: "+url );
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.connect();
-
-        try {
-
-            InputStream in = urlConnection.getInputStream();
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                Log.d("NetworkUtils", "getResponseFromHttpUrl:  HAS Input");
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
-        }
-    }
 
     @Override
     public void onRecipeSelected(int position) {
--- app/src/main/java/com/example/android/bakingapp/UI/RecipesListFragment.java
+++ app/src/main/java/com/example/android/bakingapp/UI/RecipesListFragment.java
@@ -2,16 +2,31 @@ package com.example.android.bakingapp.UI;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.android.bakingapp.POJOs.Recipe;
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.Utils.RecipeJsonUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +44,14 @@ public class RecipesListFragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private ArrayList<Recipe> recipes;
+    @BindView(R.id.rv_list_recipe)
+    RecyclerView mRecylerView;
+
+    @BindView(R.id.pb_loading)
+    ProgressBar mProressBar;
+
+
+    RecipeAdapter mAdapter;
     //private String mParam2;
 
     private OnFragmentInteractionListener mListener;
@@ -52,6 +75,7 @@ public class RecipesListFragment extends Fragment {
         args.putString(ARG_LIST_RECIPES, param1);
         //args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -70,8 +94,56 @@ public class RecipesListFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_recipes_list, container, false);
+        ButterKnife.bind(this,view);
+        ArrayList<Recipe> dummy = new ArrayList<>();
+        dummy.add(new Recipe(1,"Hala",1));
+        mAdapter = new RecipeAdapter(dummy,getContext());
+        mRecylerView.setAdapter(mAdapter);
+        
+
+
+        URL url=null;
+        try {
+            url = new URL("https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json");
+        }
+        catch (MalformedURLException mue){
+            mue.printStackTrace();
+        }
+        //new LoadListFragment().execute(url);
 
         return view;
+    }
+
+      class LoadListFragment extends AsyncTask<URL,Void,ArrayList<Recipe>>{
+
+        protected void onPreExecute() {
+            mProressBar.setVisibility(ProgressBar.VISIBLE);
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ArrayList<Recipe> doInBackground(URL... urls) {
+            URL url = urls[0];
+            ArrayList<Recipe> recipes =null;
+            try {
+                String jsonResult = getResponseFromHttpUrl(url);
+                recipes  = RecipeJsonUtils.reviewJsonParse(jsonResult);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return recipes;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Recipe> recipes) {
+            super.onPostExecute(recipes);
+            mProressBar.setVisibility(View.INVISIBLE);
+            mAdapter = new RecipeAdapter(recipes,getContext());
+            mRecylerView.setAdapter(mAdapter);
+
+
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -81,16 +153,17 @@ public class RecipesListFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    //Todo Uncomment This Method and implement it
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
@@ -111,5 +184,46 @@ public class RecipesListFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int noOfColumns = 0;
+        Log.e("XXXX", "dpWidth = : "+dpWidth );
+        if(dpWidth <=599){
+            noOfColumns=1;
+        }else {
+            int scalingFactor = 200;
+            noOfColumns = (int) (dpWidth / scalingFactor);
+            if(noOfColumns < 2)
+                noOfColumns = 2;
+        }
+        return noOfColumns;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        Log.d("XXXXXXXX", "getResponseFromHttpUrl: "+url );
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.connect();
+
+        try {
+
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                Log.d("NetworkUtils", "getResponseFromHttpUrl:  HAS Input");
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
