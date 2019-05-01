184aaa854f0744d4c547272bb56bf890aa632368
Barnawi <ab.barnawi@gmail.com>
2019-05-01 23:17:08 +0300

    Implemented Listener in RecipeListFragment

---
+++ app/src/main/java/com/example/android/bakingapp/POJOs/Data/RecipesHolder.java
@@ -0,0 +1,8 @@
+package com.example.android.bakingapp.POJOs.Data;
+
+/**
+ * Created by Kingdom on 5/1/2019.
+ */
+
+public class Recipes {
+}
---
+++ app/src/main/java/com/example/android/bakingapp/UI/DetailedRecipeAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.bakingapp.UI;
+
+/**
+ * Created by Kingdom on 5/1/2019.
+ */
+
+public class DetailedRecipeAdapter {
+}
